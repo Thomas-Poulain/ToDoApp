859cfe1ef028699dea6ef172c0a91b2d3f121160
Thomas-Poulain <e2202656@etud.univ-ubs.fr>
2023-03-10 21:34:06 +0100

    ajout

--- src/js/toDoList.js
+++ src/js/toDoList.js
@@ -4,9 +4,18 @@ createApp({
     data() {
         return {
             tasks: [ 
-                {desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black"},
-                {desc: "fd", dateStart: "2022-22-02", dateEnd: "2022-22-22", progress: "TODO", priority: "black"},
-                {desc: "feee", dateStart: "2022-22-02", dateEnd: "2022-22-22", progress: "TODO", priority: "black"}
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:2, desc: "fd", dateStart: "2022-22-02", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: false},
+                {id:3, desc: "fegggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggge", dateStart: "2022-22-02", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true},
+                {id:1, desc: "feur", dateStart: "2022-22-000002", dateEnd: "2022-22-22", progress: "TODO", priority: "black", completed: true}
             ]
         }
     },
@@ -19,11 +28,35 @@ createApp({
             } else {
                 this.priority = "🟢"
             }
-            this.tasks.push({desc: this.desc, dateStart:this.dateStart, dateEnd:this.dateEnd, progress:this.progress, priority:this.priority});
+
+            this.tasks.push({
+                id:this.tasks.length + 1, 
+                desc: this.desc, 
+                dateStart:this.dateStart, 
+                dateEnd:this.dateEnd, 
+                progress:this.progress, 
+                priority:this.priority, 
+                completed:false
+            });
         },
         deleteAll: function(){
-            this.tasks = {}
+            this.tasks = []
+        },
+        removeTask: function(index){
+            this.tasks.splice(index, 1);
+        },
+        removeCompleted: function(){
+            this.tasks = this.tasks.filter(this.inProgress);
+        },
+        getUnended(task) {
+            return ! this.isCompleted(task);
+        },
+        isCompleted(task) {
+            return task.completed;
         }
+
+
+
         
     }
 }).mount('.app')
